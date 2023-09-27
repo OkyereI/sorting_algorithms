9a59cbd721d967670d34870f970962fdfe00fc11
isaac okyere <122814014+OkyereI@users.noreply.github.com>
2023-09-27 00:18:23 +0000

    Update 105-radix_sort.c

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,89 +1,126 @@
 #include "sort.h"
-#include <stdio.h>
-/**
-* getMax - A utility function to get maximum value in arr[]
-* @arr: array
-* @n: size of the array
-* Return: array
-*/
-int getMax(int *arr, int n)
-{
-	int i, max = arr[0];
 
-	for (i = 1; i < n; i++)
-		if (arr[i] > max)
-			max = arr[i];
+/**
+ * get_max - get the maximum value of an array of integers
+ *
+ * @array: array from which to get the maximum value
+ * @size: size of the array
+ *
+ * Return: the maximum value of array
+ */
+static int get_max(int *array, size_t size)
+{
+	size_t index = 0;
+	int max = 0;
+
+	while (index < size)
+	{
+		if (array[index] > max)
+			max = array[index];
+		index += 1;
+	}
 	return (max);
 }
 
 /**
-* countSort - A function to do counting sort of arr[] according to
-* the digit represented by exp.
-* @arr: array
-* @n: size of the array
-* @exp: exp is 10^i
-* @output: array to save the temporary values
-*/
-void countSort(int *arr, size_t n, int exp, int *output)
+ * list_push - push a value onto the end of a list of integers
+ *
+ * @list: pointer to the list of integers to which to push the value
+ * @n: the value to push into the list
+ */
+static void list_push(listint_t **list, int n)
 {
-	int i;
-	int count[10] = {0};
+	listint_t *last = *list;
+	listint_t *node = malloc(sizeof(listint_t));
 
-	/* Store count of occurrences in count[] */
-	for (i = 0; i < (int)n; i++)
-		count[(arr[i] / exp) % 10]++;
+	node->n = n;
 
-	/*
-	* Change count[i] so that count[i] now contains actual
-    * position of this digit in output[]
-	*/
-	for (i = 1; i < 10; i++)
-		count[i] += count[i - 1];
-
-	/* Build the output array */
-	for (i = n - 1; i >= 0; i--)
+	if (last)
 	{
-		output[count[(arr[i] / exp) % 10] - 1] = arr[i];
-		count[(arr[i] / exp) % 10]--;
+		while (last->next)
+			last = last->next;
+		node->next = NULL;
+		node->prev = last;
+		last->next = node;
 	}
-
-	/*
-	* Copy the output array to arr[], so that arr[] now
-    * contains sorted numbers according to current digit
-	*/
-	for (i = 0; i < (int)n; i++)
-		arr[i] = output[i];
-	/*print_array(arr, n);*/
+	else
+	{
+		node->next = NULL;
+		node->prev = NULL;
+		*list = node;
+	}
 }
 
 /**
-* radix_sort - The main function to that sorts arr[]
-* of size n using Radix Sort
-* @array: array
-* @size: size of the array
-*/
+ * list_pop - pop the first value from a list of integers
+ *
+ * @list: pointer to the list of integers from which to pop a value
+ *
+ * Return: the first value in a list of integers
+ */
+static int list_pop(listint_t **list)
+{
+	listint_t *node = *list;
+	int n = node->n;
+
+	*list = node->next;
+	free(node);
+	return (n);
+}
+
+/**
+ * alloc_buckets - dynamically allocate buckets for each digit
+ *
+ * @array: array of integers to be sorted
+ * @size: number of elements in the array to be sorted
+ * @factor: factor of digit on which to sort
+ * @buckets: pointer to array of pointers to buckets
+ */
+static void alloc_buckets(
+	const int *array, size_t size, int factor, listint_t *(*buckets)[10]
+)
+{
+	int n = 0;
+	size_t index = 0;
+	size_t bucket = 0;
+
+	while (index < size)
+	{
+		n = array[index++];
+		bucket = (n / factor) % 10;
+		list_push(&(*buckets)[bucket], n);
+	}
+}
+
+/**
+ * radix_sort - sort an array of integers using the LSD radix sort algorithm
+ *
+ * @array: array of integers to be sorted
+ * @size: number of elements in the array to be sorted
+ */
 void radix_sort(int *array, size_t size)
 {
-	/* Find the maximum number to know number of digits */
-	int exp, maximum = 0;
-	int *output = '\0'; /* output array should be n(size) */
+	listint_t *buckets[10] = {0};
+	size_t bucket = 0;
+	size_t index = 0;
+	int max = 0;
+	int factor = 1;
 
-	if (array == '\0' || size < 2)
-		return;
-
-	maximum = getMax(array, size);
-	output = malloc(size * sizeof(int));
-	if (output == '\0')
-		return;
-	/*
-	* Do counting sort for every digit. Note that instead
-    * of passing digit number, exp is passed. exp is 10^i
-    * where i is current digit number
-	*/
-	for (exp = 1; maximum / exp > 0; exp *= 10)
+	if (array && size > 1)
 	{
-		countSort(array, size, exp, output);
-		print_array(array, size);
+		for (max = get_max(array, size); max / factor; factor *= 10)
+		{
+			alloc_buckets(array, size, factor, &buckets);
+			for (bucket = 0, index = 0; bucket < 10; bucket += 1)
+			{
+				while (buckets[bucket])
+				{
+					array[index++] = list_pop(
+						&buckets[bucket]
+					);
+				}
+			}
+			print_array(array, size);
+		}
 	}
-	free(output);
 }
